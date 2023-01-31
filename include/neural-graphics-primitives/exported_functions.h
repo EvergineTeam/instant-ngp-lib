3ef9fc65a7b2e89ebbc115295f16b890f85667ae
Javier Carnero <jcarnero@plainconcepts.com>
2023-01-31 15:06:08 +0100

    added fov functions. other parameters exposed

--- include/neural-graphics-primitives/exported_functions.h
+++ include/neural-graphics-primitives/exported_functions.h
@@ -16,8 +16,16 @@ EXPORT_API void INTERFACE_API nerf_initialize(const char* scene, const char* che
 EXPORT_API void INTERFACE_API nerf_deinitialize();
 
 EXPORT_API unsigned int INTERFACE_API nerf_create_texture(int width, int height);
-EXPORT_API void INTERFACE_API nerf_update_texture(float* camera_matrix, unsigned int handle);
+EXPORT_API void INTERFACE_API nerf_update_texture(float *camera_matrix0,
+												  unsigned int handle,
+												  float *camera_matrix1 = 0,
+												  float *rolling_shutter = 0,
+												  int is_rgb = 1);
 EXPORT_API void INTERFACE_API nerf_update_aabb_crop(float* min_vec, float* max_vec);
+EXPORT_API float nerf_fov();
+EXPORT_API void nerf_set_fov(float val);
+EXPORT_API Eigen::Vector2f nerf_fov_xy();
+EXPORT_API void nerf_set_fov_xy(float *xy)
 EXPORT_API void INTERFACE_API nerf_destroy_texture(unsigned int handle);
 EXPORT_API void INTERFACE_API nerf_reset_camera();
 
